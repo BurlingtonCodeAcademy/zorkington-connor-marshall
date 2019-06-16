c5fe36be2a436908924f26c035b846ed3b9effe8
DoctorGrim <estlund.marshall@gmail.com>
2019-06-15 22:31:19 -0400

    ade items objects insted of arrays and broke a bunch of stuff

--- index.js
+++ index.js
@@ -9,37 +9,97 @@ function ask(questionText) {
     readlineInterface.question(questionText, resolve);
   });
 }
+/////TODO/////
+//convert items into objects and give them names, descriptions, pickup ability and pickup text
+//same for room features
+//add hunger status to user
 
-class rooms {
-  constructor(description, items, puzzles, connections, errorStatement) {
-    this.description = description;
+class Room {
+  constructor(
+    descriptions,
+    items,
+    puzzles,
+    connections,
+    thing,
+    errorStatement
+  ) {
+    this.description = descriptions;
     this.roomInventory = items;
     this.puzzles = puzzles;
     this.connections = connections;
-    this.error=errorStatement;
+    this.things = thing;
+    this.error = errorStatement;
   }
 }
+class Thing {
+  constructor(name, altNames, description, pickUpText, takeable) {
+    this.name = name;
+    this.altNames = altNames;
+    this.description = description;
+    this.pickUpText = pickUpText;
+    this.takeable = takeable;
+  }
+}
+let puzzles = {
+  keyCode: function keyCode(code) {
+    console.log(code + "25");
+    if (code === "12345" && mainSt.connections != "foyer") {
+      mainSt.connections.push("foyer");
+      console.log("the door unlocked");
+    } else if (mainSt.connections == "foyer") {
+      console.log("the door is already unlocked");
+    } else {
+      console.log("BZZZZ wrong code");
+    }
+  },
+  testPuzzle: function() {}
+};
+///things////
+let paper = new Thing(
+  "News Paper",
+  ["news paper", "paper", "tabloid", "old persons iphone"],
+  "A basic news paper. its probably fake news",
+  "you don't know why but you take the dirty news paper off the ground",
+  true
+);
 
-let mainSt = new rooms(
+let sevenDays = new Thing(
+  "Seven Days",
+  ["seven days", "paper", "tabloid", "old persons iphone", "news paper"],
+  "A copy of Seven Days, Vermont's Alt-Weekly",
+  "You pick up the paper and leaf through it looking for comics and ignoring the articles, just like everybody else does.",
+  true
+);
+let sign = new Thing(
+  "Sign",
+  ["sign"],
+  `The sign says "Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.`,
+  "That would be selfish. How will other students find their way?",
+  false
+);
+
+////rooms/////
+let mainSt = new Room(
   `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`,
-  ["paper"],
-  keyCode,
-  ["foyer"],
-  "The sign is bolted and you're too weak to remove it."
+On the door is a handwritten sign.\n`,
+  [paper, sign],
+  puzzles.keyCode,
+  [],
+  {
+    sign:
+      "Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345.",
+    door: "The door is locked. There is a keypad on the door handle."
+  }
 );
-
-//console.log(mainSt);
-
 // after entering keyCode, PC enters the foyer
-let foyer = new rooms(
+let foyer = new Room(
   `Foyer.
 You find yourself in the stairwell- or "foyer" as it is known in some cultures. 
 As you think of the word "foyer" you smile to yourself and compliment your refined cultural pallete, "Go, me!" you think. 
 Taking a mental inventory of your surroundings, you notice a copy of the 'Seven Days' newspaper folded up in the corner`,
-  ["Seven Days"],
+  sevenDays,
   null,
   ["mainSt", "upstairs"],
   ""
@@ -49,7 +109,7 @@ Taking a mental inventory of your surroundings, you notice a copy of the 'Seven 
 //console.log(foyer);
 
 // from the foyer, PC has the option to walk upstairs- this is the path to progression. PC can go back outside, but that'd be silly.
-let upstairs = new rooms(
+let upstairs = new Room(
   `Upstairs.
 You are at the top of the stairwell facing the balcony door, 
 the bathroom door to your right, and to your right is the Burlington Code Academy classroom. Through the window of the balcony door, you see somebody puffing their Juul outside.`,
@@ -57,10 +117,10 @@ the bathroom door to your right, and to your right is the Burlington Code Academ
   null,
   ["bathroom", "classroom", "balcony"]
 );
-// from here, PC can either walk through balcony door, enter bathroom, or go to classroom. All 3 are different and unique rooms.
+// from here, PC can either walk through balcony door, enter bathroom, or go to classroom. All 3 are different and unique Room.
 
 // going to the bathroom will yield a dead-end at this time. the only available action is to 'go back' to 'upstairs'
-let bathroom = new rooms(
+let bathroom = new Room(
   `Bathroom.
 You decide to head into the bathroom for reasons unknown to anyone other than yourself. 
 You punch in the code... '0202' and turn the handle.
@@ -73,7 +133,7 @@ Best to walk away now...`,
 );
 
 //
-let balcony = new rooms(
+let balcony = new Room(
   `Balcony.
 You walk through the door leading to the balcony. 
 You are standing outside. Your classmate Connor is puffing on his Juul. 
@@ -84,7 +144,7 @@ You notice TA Bob's hat laying on the ground...`,
 );
 
 //
-let classroom = new rooms(
+let classroom = new Room(
   `Classroom.
 You open the door to the classroom and walk in. 
 You are standing in the classroom.
@@ -94,7 +154,7 @@ Joshua is presenting a lecture to the class...`["chair"],
 );
 
 //
-let mrMikes = new rooms(
+let mrMikes = new Room(
   `You walk down the street heading east towards VCET.
   You arrive at the entrace to Mr. Mikes Pizza and walk in.
   The thick aroma of gluten and mozzarella fills your nostrils as you walk up to the counter.
@@ -122,8 +182,10 @@ const acceptbleCommands = {
     "straight",
     "onward"
   ],
-  activateExternal: ["open", "talk", "speak", "buy"],
-  checkIventory: ["inventory", "check inventory", "i"]
+  activateExternal: ["open", "talk", "speak", "buy", "read"],
+  checkIventory: ["inventory", "check inventory", "i"],
+  drop: ["drop"],
+  use: ["use"]
 };
 
 const roomLookup = {
@@ -136,107 +198,95 @@ const roomLookup = {
   mrMikes: mrMikes
 };
 
-
-
 let user = {
-  inventory: ["meth"],
+  inventory: [],
   currentRoom: "mainSt",
   actions: {
-    take(input, roomInventory) {
+    take(input) {
       //delete from roomInventory and push to PC inventory
-      let found = roomInventory.find(item => item === input);
-      console.log(found);
-      if (found === undefined) {
-        console.log(`There isnt a  ${input} I can take`);
-        return;
-      }
-      user.inventory.push(found);
-      roomLookup[user.currentRoom].roomInventory = roomInventory.filter(
-        items => items !== input
-      ); //function used multiple times
-      return;
+      roomLookup[user.currentRoom].roomInventory.forEach(obj => {
+        let found = obj.altNames.find(item => item === input)
+        if (found) {
+          if (obj.takeable ===true){
+          user.inventory.push(obj);
+          console.log(obj.pickUpText);
+          roomLookup[user.currentRoom].roomInventory = roomLookup[
+            user.currentRoom
+          ].roomInventory.filter(newobj => newobj != obj); //function used multiple times
+          return;
+        }else{
+          console.log(obj.pickUpText)
+          return
+        }
+        }
+      });
     },
     drop(input) {
       //delete from PC inventory and push to roomInventory.
-      let found = user.inventory.find(item => item === input);
-      if (found === undefined) {
-        console.log(`You don't have a ${input}`);
-        return;
-      }
-      roomLookup[user.currentRoom].roomInventory.push(found);
-      user.inventory = user.inventory.filter(items => items !== input); //function used multiple times
-      return;
+      user.inventory.forEach(obj => {
+        let found = obj.altNames.find(item => item === input);
+        if (found) {
+          roomLookup[user.currentRoom].roomInventory.push(obj);
+          user.inventory = user.inventory.filter(obj2 => obj2 !== input); //function used multiple times
+          console.log(`You droped ${found} in ${user.currentRoom}`);
+          return;
+        }
+      });
     },
     use(input) {
+      roomLookup[user.currentRoom].puzzles(input);
       //use item or object?
     },
     move(input) {
       // move to new room
+      if (input === undefined) {
+        console.log("move where?");
+        return;
+      }
       let found = roomLookup[user.currentRoom].connections.find(
         room => room === input
       );
       if (input === found) {
         user.currentRoom = found;
+        console.log(`You moved to ${user.currentRoom}`);
+        return;
+      } else {
+        console.log("You can't move there");
         return;
       }
-      console.log("You can't move there");
-      return;
     },
     checkIventory() {
       console.log(user.inventory);
       return;
+    },
+    activateExternal(input) {
+      console.log(roomLookup[user.currentRoom].things.sign);
     }
   }
 };
 
 async function action() {
-  console.log("start game");
-  let input = await ask("test");
-  console.log(input);
-  //check action
-  //perform action/
-  let inputArr=input.split(' ');
-  //seperate action from input
-  let action=inputArr[0];
-  let directive=inputArr[1];
-
-  Object.keys(acceptbleCommands).forEach(function(key){
-    if (key.find(action=> action === input)){
-
-    }
-  };
-
-  // for each key in aceptableComands check each value
-  // in the array to see if it matches the first part of the input
-
+  let input = await ask(roomLookup[user.currentRoom].description);
+  checkValidInput(input);
   ///////TEST STUFF HERE//////
-
-  console.log(user.currentRoom);
-  user.actions.move("Bathroom");
-  console.log(user.currentRoom);
-
-  console.log(roomLookup[user.currentRoom].roomInventory);
-  console.log(user.inventory);
-
-  user.actions.take("Seven Days", roomLookup[user.currentRoom].roomInventory);
-
-  console.log(roomLookup[user.currentRoom].roomInventory);
-  console.log(user.inventory);
-  user.actions.drop("meth");
-
-  console.log(user.currentRoom);
-  console.log(roomLookup[user.currentRoom].roomInventory);
-  console.log(user.inventory);
-  console.log(mainSt.roomInventory);
-
   return action();
-};
+}
 action();
 
-/////////stuff to go into objects/////
-
-function keyCode(code) {
-  if (code === 12345) {
-    main.connections.push("stairs");
+function checkValidInput(userInput) {
+  input = userInput.toLowerCase();
+  let inputArr = input.split(" "); //separate action from input
+  let action = inputArr[0];
+  let directive = inputArr[1];
+  for (let action2 in acceptbleCommands) {
+    // action2 are the true actions found is the user input
+    let found = acceptbleCommands[action2].find(action3 => action3 === action);
+    if (found) {
+      user.actions[action2](directive);
+      return;
+    }
   }
+  console.log(`Sorry, I don't know how to ${action}.`);
+  return;
 }
+/////////stuff to go into objects/////
